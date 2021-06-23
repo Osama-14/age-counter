52deee011600e152365617b316c11f962bffdb88
Osama-14 <mohdosama42000@gmail.com>
2021-06-23 22:03:44 +0500

    osama

---
+++ app.js
@@ -0,0 +1,11 @@
+var daysH = document.getElementById("date");
+var monthH = document.getElementById("month");
+var yearH = document.getElementById("year");
+
+var showAge = document.getElementById("show-age");
+
+var btn = document.getElementById("calc.btn");
+
+var today = new Date();
+
+console.log(today)
